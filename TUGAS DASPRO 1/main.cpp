eab17786c8b30f1ae3fd7810a41eeab4fb47f444
Mordiantrirejeki <112890472+Mordiantrirejeki@users.noreply.github.com>
2022-09-09 10:51:50 +0700

    Add files via upload

---
+++ TUGAS DASPRO 1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAMA: Mordian tri rejeki\nNIM : A11.2022.14635\nASAL SEKOLAH: SMK Muhammadiyah 1 kedungtuban\n" << endl;
+    return 0;
+}
