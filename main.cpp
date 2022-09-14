e1c5486e36f39357f4644f0f93cc9709a5c2c2c9
Mordiantrirejeki <112890472+Mordiantrirejeki@users.noreply.github.com>
2022-09-14 22:21:15 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int x =20000;
+    cout << "Mordian tri rejeki\n" << x;
+    printf("\ntext");
+    /* comment
+    */
+    int u = 16;
+    float t=150.5;
+    char b='L';
+    string ub = "XXL";
+    cout << "\nUmur Saya:" << u;
+    cout << "\nTinggi Saya:" << t;
+    cout << "\nUkuran Baju:" << b;
+    cout << "\nUkuran Kaos:" << ub;
+    return 0;
+}
