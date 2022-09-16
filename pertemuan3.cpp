57fe3d1470531c7807556b4753da13722b0bc0a1
Mordiantrirejeki <112890472+Mordiantrirejeki@users.noreply.github.com>
2022-09-16 17:28:51 +0700

    Add files via upload

---
+++ pertemuan3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Adelfia Milka Novtagravina\n";
+    int t= 2003;
+    int u= 18;
+    float c=163.5;
+    char f= 'P';
+    string g= "M" ;
+    cout<< "\nTahun lahir saya  : " << t;
+    cout<< "\nUmur saya         : " << u;
+    cout<< "\nTinggi saya       : " << c;
+    cout<< "\nJenis kelamin saya: " << f;
+    cout<< "\nUkuran baju saya  : " << g;
+    cout<< "\n\tTerimakasih" << endl;
+
+    return 0;
+}
